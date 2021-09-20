fe5c692d09a76a77da1eeff0f4c58cb47cb6d571
Scot Lare <slare@rti.org>
2021-09-20 10:20:45 -0400

    added responsive design to Penelope for testing on phone

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -2,16 +2,6 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    screens: {
-      'xs': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'laptop': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'desktop': '1280px',
-      // => @media (min-width: 1280px) { ... }
-    },    
     screens: {xs: "540px",},
     extend: {
       fontFamily: {
