b573b8e0fe7c99d1d4a77585555b2a1b24dfb599
Scott Lare <scott.lare@gmail.com>
2021-09-26 19:18:02 -0400

    fixed up spacing and line breaks on the Lady Killer

--- docs/src/.vuepress/components/TheLadyKiller.vue
+++ docs/src/.vuepress/components/TheLadyKiller.vue
@@ -24,64 +24,137 @@
           <br />
         </div>
         <p class="text-gray-900 text-2xl p-8 mb-24 mr-56 text-center">
-          He’s brilliant and cool, and frighteningly tall He has everything you
-          want, anything at all He speaks a bit of French, his lips curl when he
-          smiles He has a glimmer in his eye, a James Dean sort of style
+          He’s brilliant and cool, and frighteningly tall
+          <br />
+          He has everything you want, anything at all
+          <br />
+          He speaks a bit of French, his lips curl when he smiles
+          <br />
+          He has a glimmer in his eye, a James Dean sort of style
           <br />
           <br />
 
-          This is not a sappy testament to my love for him Nor a silly diary
-          entry depicting how I’ll win His heart which is surely an
-          infinitesimal size No this is more a list A list of all the reasons he
-          should die
+          This is not a sappy testament to my love for him
+          <br />
+
+          Nor a silly diary entry depicting how I’ll win
+          <br />
+
+          His heart which is surely an infinitesimal size
+          <br />
+
+          No this is more a list
+          <br />
+
+          A list of all the reasons he should die
           <br />
           <br />
 
-          One he wakes up with a new girl every day Two he breaks hearts when he
-          refuses to stay Three he’s nothing but a living walking cliche Four
-          I’m pretty sure he killed someone last May
+          One he wakes up with a new girl every day
+          <br />
+
+          Two he breaks hearts when he refuses to stay
+          <br />
+
+          Three he’s nothing but a living walking cliche
+          <br />
+
+          Four I’m pretty sure he killed someone last May
           <br />
           <br />
 
-          Now I can’t be certain, so please don’t repeat But I haven’t seen
-          Daisy and it’s been twenty weeks She went on a date with him, beguiled
-          by his charm No way with those heels could she escape from any harm
+          Now I can’t be certain, so please don’t repeat
+          <br />
+
+          But I haven’t seen Daisy and it’s been twenty weeks
+          <br />
+
+          She went on a date with him, beguiled by his charm
+          <br />
+
+          No way with those heels could she escape from any harm
           <br />
           <br />
 
-          He seems so innocent, but that never fooled me One time I saw him,
-          after period three Behind the bleachers, sitting in the shade I
-          could’ve sworn he was holding a very bloody blade
+          He seems so innocent, but that never fooled me
+
+          <br />
+
+          One time I saw him, after period three
+
+          <br />
+
+          Behind the bleachers, sitting in the shade
+
+          <br />
+
+          I could’ve sworn he was holding a very bloody blade
           <br />
           <br />
 
-          I don’t know what I should do If my thoughts are even true But I don’t
-          trust him as he flirts with every girl in school Wouldn’t it be on me
-          if another went missing? What if his next victim is the blonde girl
-          he’s been kissing?
+          I don’t know what I should do
+          <br />
+
+          If my thoughts are even true
+          <br />
+
+          But I don’t trust him as he flirts with every girl in school
+          <br />
+
+          Wouldn’t it be on me if another went missing?
+          <br />
+
+          What if his next victim is the blonde girl he’s been kissing?
           <br />
           <br />
 
-          I have to stop it, maybe alert the police But as it turns out, his
-          father’s the chief I need to get him alone, which sounds frankly crazy
+          I have to stop it, maybe alert the police
+          <br />
+
+          But as it turns out, his father’s the chief
+          <br />
+
+          I need to get him alone, which sounds frankly crazy
+          <br />
           But someone has to avenge poor, guiltless Daisy
           <br />
           <br />
 
-          This part is probably not best to say aloud And once I get my thoughts
-          out, I’ll burn this paper down The plan is to smile and wear ruby red
-          lips The plan is to beg him for one little kiss
+          This part is probably not best to say aloud
+          <br />
+
+          And once I get my thoughts out, I’ll burn this paper down
+          <br />
+
+          The plan is to smile and wear ruby red lips
+          <br />
+
+          The plan is to beg him for one little kiss
           <br />
           <br />
 
-          I don’t know how dangerous he is, what he’s capable of I don’t know
-          what he’ll do when push comes to shove But he also doesn’t know a
-          thing about me And just how dangerous I can really be
+          I don’t know how dangerous he is, what he’s capable of
+          <br />
+
+          I don’t know what he’ll do when push comes to shove
+          <br />
+
+          But he also doesn’t know a thing about me
+          <br />
+
+          And just how dangerous I can really be
           <br />
           <br />
-          I am angry and ruthless, and so very determined I am merciless and
-          cruel, with no patience for vermin So he speaks French? So he has a
-          glimmer in his eye? He is about to kiss everything goodbye
+          I am angry and ruthless, and so very determined
+          <br />
+
+          I am merciless and cruel, with no patience for vermin
+          <br />
+
+          So he speaks French? So he has a glimmer in his eye?
+          <br />
+
+          He is about to kiss everything goodbye
         </p>
         <br />
       </div>
